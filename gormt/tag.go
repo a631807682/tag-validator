f375bbb1144c618dade1eca1a4da26f0bb02f826
a631807682 <631807682@qq.com>
2022-07-29 16:56:56 +0800

    feat(wip): check gorm tag name

--- gormt/tag.go
+++ gormt/tag.go
@@ -29,3 +29,56 @@ func format(gtag string) string {
 
 	return gtag
 }
+
+type TagType int8
+
+const (
+	// table column definition
+	Table TagType = iota + 1
+	// column permission
+	Permission
+	// gorm internal implementation
+	Internal
+	// schema relation
+	Relation
+)
+
+// https://gorm.io/docs/models.html
+var tagNamesMap = map[string]TagType{
+	"column":                 Table,
+	"type":                   Table,
+	"size":                   Table,
+	"primaryKey":             Table,
+	"primary_key":            Table, // same with primaryKey
+	"unique":                 Table,
+	"default":                Table,
+	"precision":              Table,
+	"scale":                  Table,
+	"not null":               Table,
+	"autoIncrement":          Table,
+	"autoIncrementIncrement": Table,
+	"index":                  Table,
+	"uniqueIndex":            Table,
+	"comment":                Table,
+
+	"serializer":     Internal,
+	"embedded":       Internal,
+	"embeddedPrefix": Internal,
+	"autoCreateTime": Internal,
+	"autoUpdateTime": Internal,
+	"check":          Internal,
+
+	"<-": Permission,
+	"->": Permission,
+	"-":  Permission,
+
+	"foreignKey":       Relation,
+	"references":       Relation,
+	"polymorphic":      Relation,
+	"many2many":        Relation,
+	"belongsto":        Relation,
+	"polymorphicValue": Relation,
+	"joinForeignKey":   Relation,
+	"joinReferences":   Relation,
+	"constraint":       Relation,
+}
